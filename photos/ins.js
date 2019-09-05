10314f8686b5a0e03f8a70545681f6665f1ffabe
kaiqiang cao <kevinswiftios@gmail.com>
2019-09-05 22:20:43 +0800

    Site updated: 2019-09-05 22:20:42

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,9 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/KevinSwiftStack/min_photos/master/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/KevinSwiftStack/photos/master/' + data.link[i];
+          console.log(minSrc);
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
